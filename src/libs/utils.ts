e7e9e30573e5fb4b9d0e8a731633e0c91288a0b2
Kocannn <dwicandraandika4@gmail.com>
2025-02-07 22:54:15 +0700

    feat:backend ssr fetch data

---
+++ src/libs/utils.ts
@@ -0,0 +1,13 @@
+export default async function ServerSideRenderedComponent(title: string) {
+    const dataVideo = await fetch('http://localhost:3000/api/v1/getVideo', {
+
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ title }),
+    }
+    )
+    const data = await dataVideo.json()
+    return data
+}
