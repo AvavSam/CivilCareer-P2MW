d6b731b2b8c786fc52515db5957e9852ae0e818d
AvavSam <avav2608005@gmail.com>
2025-02-12 12:27:19 +0800

    feat: add Google user image hostname to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,9 @@ const nextConfig: NextConfig = {
       {
         hostname: "platform-lookaside.fbsbx.com",
       },
+      {
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
 };
